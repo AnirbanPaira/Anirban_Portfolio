8433718e6c504be1a1f07d5c52a787643899599e
Anirban <pairaanirban123@gmail.com>
2022-10-10 22:42:06 +0530

    initial commit

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,6 @@
 Thanks for downloading this template!
 
 Template Name: iPortfolio
-Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+Template URL: linkedin.com/in/anirban-paira-3a1b531a9/iportfolio-bootstrap-portfolio-websites-template/
+Author: Anirban Paira.com
+License: linkedin.com/in/anirban-paira-3a1b531a9/license/
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 /**
  * Template Name: iPortfolio - v3.7.0
- * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
+ * Template URL: linkedin.com/in/anirban-paira-3a1b531a9/iportfolio-bootstrap-portfolio-websites-template/
+ * Author: Anirban Paira.com
+ * License: linkedin.com/in/anirban-paira-3a1b531a9/license/
  */
 (function () {
   "use strict";
